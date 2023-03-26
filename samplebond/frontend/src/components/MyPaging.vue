f67d65cff12aafcf97defddbc03dba0a14f73e25
wizmage <wizmage82@gmail.com>
2023-03-27 08:05:12 +0900

    1

--- samplebond/frontend/src/components/MyPaging.vue
+++ samplebond/frontend/src/components/MyPaging.vue
@@ -1,40 +1,52 @@
-<template>{{ curPageSize }}
+<template>
 	<ul id="paging">
-		<li><button @click="goToPrev">&laquo;</button></li>
-		<li><button @click="goPage"></button></li>
-		<li><button @click="goToNext">&raquo;</button></li>
+		<li><button @click="goToFirst">&laquo;</button></li>
+		<li><button @click="goToPrev">&lt;</button></li>
+		<li v-for="(no, i) in pageNoList" :key="i">
+			<button @click="goPage(no)"
+							v-if="no != pageNo">{{ no }}</button>
+			<span v-else>{{ no }}</span>
+		</li>
+		<li><button @click="goToNext">&gt;</button></li>
+		<li><button @click="goToLast">&raquo;</button></li>
 	</ul>
 </template>
 
 <script setup>
-import { defineProps, toRefs, watch } from 'vue';
+import { defineProps, defineEmits } from 'vue';
+
+const emit = defineEmits([ 'update:modelValue' ]);
 
 const props = defineProps({
-	totalRecordCount: { type: Number, default: 0 },
-	recordCountPerPage: { type: Number, default: 10 },
+	totalCount: { type: Number, default: 0 },
 	pageSize: { type: Number, default: 5 },
-	pageNo: { type: Number, default: 0 }
+	pageNo: { type: Number, default: 1 }
 });
 
-const firstPageNo = toRefs(props.pageNo);
-const lastPageNo = Math.ceil(props.totalRecordCount / props.recordCountPerPage);
-const curPageSize = (lastPageNo - firstPageNo) > props.pageSize ? props.pageSize : lastPageNo - firstPageNo;
+const totalPageCount = Math.ceil(props.totalCount / props.pageSize);
+const startPageNo = Math.floor(props.pageNo / props.pageSize) * props.pageSize;
+const endPageNo = Math.min(startPageNo + props.pageSize, totalPageCount);
+const pageNoList = Array(endPageNo - startPageNo).fill().map((no, i) => startPageNo + i + 1);
 
 const goPage = (pageNo) => {
-	alert(pageNo);
+	emit('update:modelValue', pageNo);
 };
 
+const goToFirst = () => {
+	goPage(1);
+}
+
 const goToPrev = () => {
-	alert(props.pageNo - 1);
+	goPage(Math.max(props.pageNo - 1, 1));
 };
 
 const goToNext = () => {
-	alert(props.pageNo + 1);
+	goPage(Math.min(props.pageNo + 1, totalPageCount));
 };
 
-watch(() => props.paging, (newValue) => {
- 	console.log(newValue);
-});
+const goToLast = () => {
+	goPage(totalPageCount);
+}
 </script>
 
 <style scoped>
--- samplebond/frontend/src/views/sample/SampleListView.vue
+++ samplebond/frontend/src/views/sample/SampleListView.vue
@@ -1,12 +1,21 @@
 <template>
 	<!-- searchbar -->
-	<div>TODO search bar</div>
+	<div>
+		<label>Y/N</label>
+		<select v-model="searchParam.useYn">
+			<option value="">전체</option>
+			<option value="Y">Y</option>
+			<option value="N">N</option>
+		</select>
+		<button @click="search">검색</button>
+	</div>
 	<!-- /searchbar -->
 	<!-- data table -->
 	<div class="table">
 		<table width="100%" border="0" cellpadding="0" cellspacing="0" summary="카테고리ID, 케테고리명, 사용여부, Description, 등록자 표시하는 테이블">
 			<caption style="visibility:hidden">카테고리ID, 케테고리명, 사용여부, Description, 등록자 표시하는 테이블</caption>
 			<colgroup>
+				<col width="60"/>
 				<col width="100"/>
 				<col width="150"/>
 				<col width="80"/>
@@ -14,53 +23,74 @@
 				<col width="60"/>
 			</colgroup>
 			<tr>
-				<th align="center">title.sample.id</th>
-				<th align="center">title.sample.name</th>
-				<th align="center">title.sample.useYn</th>
-				<th align="center">title.sample.description</th>
-				<th align="center">title.sample.regUser</th>
+				<th align="center">no</th>
+				<th align="center">id</th>
+				<th align="center">name</th>
+				<th align="center">useYn</th>
+				<th align="center">description</th>
+				<th align="center">regUser</th>
 			</tr>
-			<tr v-for="sample in state.dataList" :key="sample.id">
+			<tr v-for="(sample, i) in sampleList" :key="i">
+				<td>{{ tableRowNo() }}</td>
 				<td align="center" class="listtd"><router-link :to="`/sampleDetail/${sample.id}`">{{ sample.id }}</router-link></td>
 				<td align="left" class="listtd">{{ sample.name }}</td>
 				<td align="center" class="listtd">{{ sample.useYn }}</td>
-				<td align="center" class="listtd">{{ sample.description }}</td>
+				<td align="center" class="listtd">{{ sample.useYn }}</td>
 				<td align="center" class="listtd">{{ sample.regUser }}</td>
 			</tr>
 		</table>
 	</div>
 	<!-- /data table -->
 	<!-- pagination -->
-	<my-paging :paging="state.paging"
-							@update:page="goPage"/>
+	<my-paging :page-size="pagingParam.pageSize"
+						 :page-no="pagingParam.pageNo"
+						 :total-count="pagingParam.totalCount"
+							@update:modelValue="goPage"/>
 	<!-- /pagination -->
 </template>
 
 <script setup>
-import { onMounted, reactive } from 'vue';
+import { onMounted, ref, toRaw } from 'vue';
 import sampleDummy from '@/dummy/sampleDummy';
 import MyPaging from '@/components/MyPaging.vue';
 
-const state = reactive({
-	dataList: [],
-	searching: {},
-	paging: {
-		totalRecordCount: 0,
-		recordCountPerPage: 10,
-		pageSize: 5,
-		pageNo: 1
-	}
+const searchParam = ref({
+	useYn: '',
+	pageNo: 1,
+	pageSize: 5
 });
+
+const pagingParam = ref({
+	totalCount: 0,
+	pageSize: 5,
+	pageNo: 1
+});
+
+const sampleList = ref(null);
+
+const tableRowNo = () => {
+	return 1;
+};
+
+const search = () => {
+	console.log(searchParam.value);
+	getSampleList();
+};
 
 const goPage = (pageNo) => {
 	console.log(pageNo);
+	searchParam.value.pageNo = pageNo;
+	getSampleList();
+};
+
+const getSampleList = () => {
+	const dummy = sampleDummy.dummyList(toRaw(searchParam.value));
+	sampleList.value = dummy.resultList;
+	pagingParam.value = dummy.paging;
 };
 
 onMounted(() => {
-	const dummy = sampleDummy.dummyList(1);
-	state.dataList = dummy.resultList;
-	state.paging = dummy.paging;
-	console.log(state.paging);
+	getSampleList();
 });
 </script>
 
